a22d6ccf88673a7a61d8009f044d3cba14f85a64
Ethan Rader <rader.ethan@gmail.com>
2020-04-23 20:59:34 -0500

    show and create methods for user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,25 @@ class Api::UsersController < ApplicationController
     @users = User.all
     render "index.json.jb"
   end
+
+  def create
+    @user = User.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"],
+      password: params["password"],
+      password_confirmation: params["password_confirmation"],
+      profile_picture: params["profile_picture"],
+    )
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages }
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params["id"])
+    render "show.json.jb"
+  end
 end
