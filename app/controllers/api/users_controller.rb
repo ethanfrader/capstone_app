3e6550da9cc8ca73831263e4c82b70a975859231
Ethan Rader <rader.ethan@gmail.com>
2020-04-23 21:05:50 -0500

    user may edit first/last name and profile picture

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -24,4 +24,16 @@ class Api::UsersController < ApplicationController
     @user = User.find_by(id: params["id"])
     render "show.json.jb"
   end
+
+  def edit
+    @user = User.find_by(params["id"])
+    @user.first_name = params["first_name"] || @user.first_name
+    @user.last_name = params["last_name"] || @user.last_name
+    @user.profile_picture = params["profile_picture"] || @user.profile_picture
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     get "/users" => "users#index"
     post "/users" => "users#create"
     get "/users/:id" => "users#show"
+    patch "users/:id" => "users#edit"
   end
 end
