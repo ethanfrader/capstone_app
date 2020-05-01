d507aaba34aea270261c70d1ff78fd5979d9a9ad
Ethan Rader <rader.ethan@gmail.com>
2020-04-30 19:52:23 -0500

    authenticate user before allowing edit

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,11 @@
 class Api::UsersController < ApplicationController
+  #allows to search for all users
   def index
     @users = User.all
     render "index.json.jb"
   end
 
+  #essentially a signup
   def create
     @user = User.new(
       first_name: params["first_name"],
@@ -20,13 +22,16 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  #can show any one user
   def show
     @user = User.find_by(id: params["id"])
     render "show.json.jb"
   end
 
+  #can only edit self
   def edit
-    @user = User.find_by(id: params["id"])
+    authenticate_user
+    @user = current_user
     @user.first_name = params["first_name"] || @user.first_name
     @user.last_name = params["last_name"] || @user.last_name
     @user.profile_picture = params["profile_picture"] || @user.profile_picture
